002e09ca50b757f26c45439fef1497200cd333b4
Acer-002 <wxyora@sina.com>
2015-09-17 17:55:34 +0800

    qweqwe

--- src/waylon/action/WeChatAction.java
+++ src/waylon/action/WeChatAction.java
@@ -2,17 +2,20 @@ package waylon.action;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import waylon.util.MessageUtil;
 import waylon.util.SignUtil;
 
 public class WeChatAction extends ActionSupport {
-	
+
 	/**
 	 * 
 	 */
@@ -21,10 +24,11 @@ public class WeChatAction extends ActionSupport {
 	private String timestamp;
 	private String nonce;
 	private String echostr;
-	
 
-	public String dealWeChat(){
+
+	public String dealWeChat() throws Exception{
 		HttpServletResponse response = ServletActionContext.getResponse();
+		HttpServletRequest request = ServletActionContext.getRequest();
 		PrintWriter out = null;
 		try {
 			out = response.getWriter();
@@ -38,6 +42,18 @@ public class WeChatAction extends ActionSupport {
 		}
 		out.close();
 		out = null;
+
+		// 调用parseXml方法解析请求消息
+		Map<String, String> requestMap = MessageUtil.parseXml(request);
+		String fromUserName = requestMap.get("FromUserName");  
+		String toUserName = requestMap.get("ToUserName");  
+		// 消息类型  
+		String msgType = requestMap.get("MsgType");  
+
+
+
+
+
 		return SUCCESS;
 	}
 
---
+++ src/waylon/util/MessageUtil.java
@@ -0,0 +1,109 @@
+package waylon.util;
+
+
+import java.io.InputStream;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+/**
+ * 消息工具类
+ */
+public class MessageUtil {
+
+	/**
+	 * 返回消息类型：文本
+	 */
+	public static final String RESP_MESSAGE_TYPE_TEXT = "text";
+
+	/**
+	 * 返回消息类型：音乐
+	 */
+	public static final String RESP_MESSAGE_TYPE_MUSIC = "music";
+
+	/**
+	 * 返回消息类型：图文
+	 */
+	public static final String RESP_MESSAGE_TYPE_NEWS = "news";
+
+	/**
+	 * 请求消息类型：文本
+	 */
+	public static final String REQ_MESSAGE_TYPE_TEXT = "text";
+
+	/**
+	 * 请求消息类型：图片
+	 */
+	public static final String REQ_MESSAGE_TYPE_IMAGE = "image";
+
+	/**
+	 * 请求消息类型：链接
+	 */
+	public static final String REQ_MESSAGE_TYPE_LINK = "link";
+
+	/**
+	 * 请求消息类型：地理位置
+	 */
+	public static final String REQ_MESSAGE_TYPE_LOCATION = "location";
+
+	/**
+	 * 请求消息类型：音频
+	 */
+	public static final String REQ_MESSAGE_TYPE_VOICE = "voice";
+
+	/**
+	 * 请求消息类型：推送
+	 */
+	public static final String REQ_MESSAGE_TYPE_EVENT = "event";
+
+	/**
+	 * 事件类型：subscribe(订阅)
+	 */
+	public static final String EVENT_TYPE_SUBSCRIBE = "subscribe";
+
+	/**
+	 * 事件类型：unsubscribe(取消订阅)
+	 */
+	public static final String EVENT_TYPE_UNSUBSCRIBE = "unsubscribe";
+
+	/**
+	 * 事件类型：CLICK(自定义菜单点击事件)
+	 */
+	public static final String EVENT_TYPE_CLICK = "CLICK";
+
+
+	@SuppressWarnings("unchecked")
+	public static Map<String, String> parseXml(HttpServletRequest request) throws Exception {
+
+		Map<String, String> map = new HashMap<String, String>();
+
+
+		InputStream inputStream = request.getInputStream();
+
+		SAXReader reader = new SAXReader();
+		Document document = reader.read(inputStream);
+		// 得到xml根元素
+		Element root = document.getRootElement();
+		// 得到根元素的所有子节点
+		List<Element> elementList = root.elements();
+
+		// 遍历所有子节点
+		for (Element e : elementList)
+			map.put(e.getName(), e.getText());
+
+		// 释放资源
+		inputStream.close();
+		inputStream = null;
+
+		return map;
+	}
+
+}
+ 
