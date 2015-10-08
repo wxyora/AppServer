22fad1ace65451649782da684e6b724b4d67619e
Acer-002 <wxyora@sina.com>
2015-10-08 18:04:30 +0800

    qweqwe

---
+++ src/waylon/domain/WeChatUserDetailInfo.java
@@ -0,0 +1,44 @@
+package waylon.domain;
+
+public class WeChatUserDetailInfo {
+	
+	private String openId;
+	private String nickName;
+	private String sex;
+	private String subscribeDate;
+	private String city;
+	
+	public String getOpenId() {
+		return openId;
+	}
+	public void setOpenId(String openId) {
+		this.openId = openId;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getSubscribeDate() {
+		return subscribeDate;
+	}
+	public void setSubscribeDate(String subscribeDate) {
+		this.subscribeDate = subscribeDate;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+
+}
